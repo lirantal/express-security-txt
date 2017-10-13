9ed727194c0891a59c98aa6276601801951281c4
Liran Tal <liran.tal@gmail.com>
2017-10-13 23:05:49 +0300

    tests(validate): increasing code coverage tests for validating policy

--- __tests__/validatePolicy.test.js
+++ __tests__/validatePolicy.test.js
@@ -12,10 +12,74 @@ test('validate doesnt throw an error on provided fields', () => {
   expect(res).toBe(true)
 })
 
+test('validate successfully when only mandatory properties provided', () => {
+  const options = {
+    contact: 'email@example.com',
+    disclosure: 'full',
+  }
+
+  expect(() => securityTxt.validatePolicyFields(options)).not.toThrow()
+})
+
 test('validate fails when disclosure is non-standard', () => {
   const options = {
     contact: 'email@example.com',
     disclosure: 'abc'
+  }
+
+  expect(() => securityTxt.validatePolicyFields(options)).toThrow()
+})
+
+test('validate fails when options is not an object', () => {
+  const options = ''
+
+  expect(() => securityTxt.validatePolicyFields(options)).toThrow()
+})
+
+test('validate fails when no contact property provided', () => {
+  const options = {
+    disclosure: 'full',
+    encryption: 'https://www.mykey.com/pgp-key.txt',
+  }
+
+  expect(() => securityTxt.validatePolicyFields(options)).toThrow()
+})
+
+test('validate fails when no disclosure property provided', () => {
+  const options = {
+    contact: 'email@example.com',
+    encryption: 'https://www.mykey.com/pgp-key.txt',
+  }
+
+  expect(() => securityTxt.validatePolicyFields(options)).toThrow()
+})
+
+test('validate fails when encryption property is used without https', () => {
+  const options = {
+    contact: 'email@example.com',
+    disclosure: 'full',
+    encryption: 'http://www.mykey.com/pgp-key.txt',
+  }
+
+  expect(() => securityTxt.validatePolicyFields(options)).toThrow()
+})
+
+test('validate fails when encryption property is not a string', () => {
+  const options = {
+    contact: 'email@example.com',
+    disclosure: 'full',
+    encryption: {}
+  }
+
+  expect(() => securityTxt.validatePolicyFields(options)).toThrow()
+})
+
+test('validate fails when acknowledgement property is not a string', () => {
+  const options = {
+    contact: 'email@example.com',
+    disclosure: 'full',
+    encryption: '',
+    acknowledgement: {}
   }
 
   expect(() => securityTxt.validatePolicyFields(options)).toThrow()
