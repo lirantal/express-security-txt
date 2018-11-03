b09adbabe96e589f555b0f11cc446c013cca490b
jokebookservice1 <jokebookyeye@gmail.com>
2018-11-03 13:16:36 +0000

    feat: Allow comments to be arrays
    Whereas #formatSecurityPolicy could handle an array of comments (which would then be glued together
    over multiple lines -- prefixed with the hash symbol to be compliant with the specification), the
    validation schema was not aware of this: and required comments be strings. We also add tests to
    ensure comments can always be arrays. Note: it might be good to factor out the schema for what a
    'comment' is, so we only need to make a modification in one place in future.

--- __tests__/formatPolicy.test.js
+++ __tests__/formatPolicy.test.js
@@ -108,11 +108,11 @@ test('formats successfully with comments', () => {
       },
       {
         value: 'https://b.example.com',
-        comment: 'c'
+        comment: ['c', 'h', 'i\nj']
       },
       'https://c.example.com'
     ],
-    _prefixComment: 'a',
+    _prefixComment: ['a', 'z', 'x\ny'],
     _postfixComment: 'd'
   }
 
@@ -120,10 +120,16 @@ test('formats successfully with comments', () => {
 
   expect(res).toBe(
     '# a\n' +
+    '# z\n' +
+    '# x\n' +
+    '# y\n' +
     '# b\n' +
     'Contact: tel:+123\n' +
     'Encryption: https://a.example.com\n' +
     '# c\n' +
+    '# h\n' +
+    '# i\n' +
+    '# j\n' +
     'Encryption: https://b.example.com\n' +
     'Encryption: https://c.example.com\n' +
     '# d\n'
